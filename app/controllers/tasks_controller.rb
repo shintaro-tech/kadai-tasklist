619f2e12a4f2d3fc9f5459935961f4d5089c36bb
shintaro <ryoutenabe0420@gmail.com>
2021-03-11 22:31:16 +0000

    add pagenation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
-    before_action :set_message, only: [:show, :edit, :update, :destroy]
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
 
     def index
-        @tasks = Task.all
+        @tasks = Task.order(id: :desc).all.page(params[:page]).per(5)
     end
 
     def show
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
     end
     
     def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
     end
 end
