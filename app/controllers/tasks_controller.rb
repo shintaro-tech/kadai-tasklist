347244bffeaf0d62bb3c8b052d422929a41bb3b3
shintaro <ryoutenabe0420@gmail.com>
2021-03-29 13:42:47 +0000

    correct destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
         @task.destroy
 
         flash[:success] = 'タスク は正常に削除されました'
-        redirect_back(fallback_location: root_path)
+        redirect_back(fallback_location: root)
     end
     
     
