f115d36e6e0c0e81a2d8d9d57e01c9bffafaba56
shintaro <ryoutenabe0420@gmail.com>
2021-03-31 06:29:07 +0000

    delete route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'toppages/index'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
@@ -13,5 +12,5 @@ Rails.application.routes.draw do
     
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :create]
 end
